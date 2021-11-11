68b4ce24d9f587502f012ce3bf3a1a589b3ff96d
Sebastião Alves <tiaomain@gmail.com>
2021-11-11 17:27:15 -0300

    adicionada a página quem somos

---
+++ pages/quemSomos.php
@@ -0,0 +1,3 @@
+<div>
+    Sou um entusiasta e estudante na área da tecnologia, a qual não pararei mais de estudar, já entendo sobre algoritmo e lógica de programação, com essa base consegui aprender o desenvolvimento back-end, onde estou sempre aprendendo e aplicando meus conhecimentos.
+</div>

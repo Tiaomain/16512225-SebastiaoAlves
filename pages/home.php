5098dd187107726a4a2c86737a5ac8b700c772c0
Sebastião Alves <tiaomain@gmail.com>
2021-11-12 11:36:57 -0300

    Inserção de serviços prestados

--- pages/home.php
+++ pages/home.php
@@ -1,4 +1,11 @@
 <div>
     <h1>Bem vindo à SoftTião!</h1>
-    <p>Atualmente presto serviços de elaboração de sistemas de gerenciamento de tarefas personalizado(CMS)
+    <p>Atualmente presto serviços de elaboração de sistemas de :
+        <ul>
+            <li><a href="#">Gerenciamento de tarefas personalizado(CMS)</a></li>
+            <li><a href="#">Personalização de sites</a></li>
+            <li><a href="#">E-commerce</a></li>
+            <li><a href="#">Pernosalização de API RESTful</a></li>
+        </ul>
+    </p>
 </div>

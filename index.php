e1c6b693293e5cf33c9ad458f773b86bbe7af242
Sebastião Alves <tiaomain@gmail.com>
2021-11-12 12:03:08 -0300

    Inserindo as classes para estilização

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en, pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,14 +8,17 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <title>Desenvolvedor Iniciante</title>
 </head>
+
 <body>
     <header>
-        <nav>
-            <div class="logo"><a href="index.php">Sebastião</a></div>
-            <ul class="menu">
-                <li><a href="index.php?pagina=pages/quemSou.php">Quem somos</a></li>
-                <li><a href="index.php?pagina=pages/contato.php">Contato</a></li>
-            </ul>
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+            <div>
+                <a class="navbar-brand" href="index.php">Sebastião</a></div>
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="index.php?pagina=pages/quemSou.php">Quem sou</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?pagina=pages/contato.php">Contato</a></li>
+                </ul>
+            </div>
         </nav>
     </header>
     <main>
@@ -33,4 +37,5 @@
         <p>RA : <strong>1651222-5</strong></p>
     </footer>
 </body>
+
 </html>

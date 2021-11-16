06c1fe1f506098db04c7405359f2e0e90dfd79c6
Sebastião Alves <tiaomain@gmail.com>
2021-11-16 17:11:56 -0300

    Inseridas as informações sobre o serviço de API.

--- pages/pageAPI.php
+++ pages/pageAPI.php
@@ -1,4 +1,9 @@
-
+<p>
+    API REST, também chamada de API RESTful, é uma interface de programação de aplicações (API ou API web) que está em conformidade com as restrições do estilo de arquitetura REST, permitindo a interação com serviços web RESTful. REST é a sigla em inglês para "Representational State Transfer", que em português significa tansferência de estado representacional.
+</p>
+<p>
+    Uma API é um conjunto de definições e protocolos usado no desenvolvimento e na integração de aplicações. Às vezes, as APIs são descritas como um contrato entre um provedor e um usuário de informações, estabelecendo o conteúdo exigido pelo consumidor (a chamada) e o conteúdo exigido pelo produtor (a resposta). Por exemplo, o design da API de um serviço meteorológico pode especificar que o usuário forneça um CEP e o produtor responda em duas partes, a primeira contendo a temperatura mais elevada e a segunda com a temperatura mais baixa.  
+</p>
 
 <img src="images/imgAPI.png" alt="image API template">
 

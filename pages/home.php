8b3f0999ccf9d0aaf1bf3165b490469d9420ec02
Sebastião Alves <tiaomain@gmail.com>
2021-11-16 17:07:40 -0300

    Inserido o link e o conteúdo do serviço de API

--- pages/home.php
+++ pages/home.php
@@ -4,7 +4,7 @@
         <ul class="list-group" >
             <li><a href="index.php?pagina=pages/pageCMS.php">Sistema de Gestão de Conteúdos (CMS) Personalizado</a></li>
             <li><a href="index.php?pagina=pages/pageEcom.php">E-commerce</a></li>
-            <li><a href="#">Personalização de API RESTful</a></li>
+            <li><a href="index.php?pagina=pages/pageAPI.php">Personalização de API REST</a></li>
         </ul>
     </p>
     <p> Visando a melhoria na qualidade do produto final, estou sempre buscando me atualizar, aprender novas metodologias, conceitos e tecnologias.</p>
---
+++ pages/pageAPI.php
@@ -0,0 +1,5 @@
+
+
+<img src="images/imgAPI.png" alt="image API template">
+
+<button type="button" class="btn btn-primary"> Voltar </button>

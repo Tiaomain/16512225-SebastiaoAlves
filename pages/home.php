a73102583ae0b0ea45c643f09b36e15b8933ebb2
Sebastião Alves <tiaomain@gmail.com>
2021-11-16 11:20:00 -0300

    Incremento do texto na página inicial.

--- pages/home.php
+++ pages/home.php
@@ -1,6 +1,6 @@
 <div>
     <h1>Bem vindo à SoftTião!</h1>
-    <p>Atualmente presto serviços de elaboração de sistemas de :
+    <p>Atualmente presto serviços de :
         <ul class="list-group" >
             <li><class="list-group-item list-group-item-action" href="#">Gerenciamento de tarefas personalizado(CMS)</a></li>
             <li><a href="#">Personalização de sites</a></li>
@@ -8,4 +8,5 @@
             <li><a href="#">Pernosalização de API RESTful</a></li>
         </ul>
     </p>
+    <p> Visando a melhoria na qualidade do produto final, estou sempre buscando me atualizar, aprender novas metodologias, conceitos e tecnologias.</p>
 </div>

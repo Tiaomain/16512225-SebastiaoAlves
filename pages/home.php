93187266d80fd301e62ab5d249c11b7b70b43910
Sebastião Alves <tiaomain@gmail.com>
2021-11-16 11:16:44 -0300

    Testes para a listagem estilizada do home

--- pages/home.php
+++ pages/home.php
@@ -1,8 +1,8 @@
 <div>
     <h1>Bem vindo à SoftTião!</h1>
     <p>Atualmente presto serviços de elaboração de sistemas de :
-        <ul>
-            <li><a href="#">Gerenciamento de tarefas personalizado(CMS)</a></li>
+        <ul class="list-group" >
+            <li><class="list-group-item list-group-item-action" href="#">Gerenciamento de tarefas personalizado(CMS)</a></li>
             <li><a href="#">Personalização de sites</a></li>
             <li><a href="#">E-commerce</a></li>
             <li><a href="#">Pernosalização de API RESTful</a></li>

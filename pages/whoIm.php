9511dd6fc92ab04106434f75a20a887cbe87ff39
Sebastião Alves <tiaomain@gmail.com>
2021-11-16 17:33:12 -0300

    Alterado o texto sobre quem sou.

--- pages/whoIm.php
+++ pages/whoIm.php
@@ -1,3 +1,9 @@
 <div>
-    Sou um entusiasta e estudante na área da tecnologia, a qual não pararei mais de estudar, já entendo sobre algoritmo e lógica de programação, com essa base consegui aprender o desenvolvimento back-end, onde estou sempre aprendendo e aplicando meus conhecimentos.
+    <p>
+        Sou um entusiasta e estudante na área da tecnologia, gosto de aprender e estou sempre em busca de novos desafios, e estou sempre em busca de novos conhecimentos.
+    <p>
+    <p>
+        Estou concluindo o curso de Sistemas para Internet, onde aprendi a utilizar as ferramentas de desenvolvimento web, como: HTML, CSS, JavaScript, PHP, Banco de Daodos, entre outros. 
+    </p>
+
 </div>

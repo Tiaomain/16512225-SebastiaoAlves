cd2aacb466571eda9e84611ab6feacc1b8f6b695
Sebastião Alves <tiaomain@gmail.com>
2021-11-16 11:29:51 -0300

    Preparando arquivo com a descrição do serviço

--- pages/home.php
+++ pages/home.php
@@ -2,7 +2,7 @@
     <h1>Bem vindo à SoftTião!</h1>
     <p>Atualmente presto serviços de :
         <ul class="list-group" >
-            <li><class="list-group-item list-group-item-action" href="#">Gerenciamento de tarefas personalizado(CMS)</a></li>
+            <li><a href="#">Sistema de Gestão de Conteúdos(CMS) Personalizado</a></li>
             <li><a href="#">Personalização de sites</a></li>
             <li><a href="#">E-commerce</a></li>
             <li><a href="#">Pernosalização de API RESTful</a></li>
